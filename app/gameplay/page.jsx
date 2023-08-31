9cfd3cbd4c60c992a74c1790ff4b84d67f0ebef7
JHoang <JHoang6900@gmail.com>
2023-08-31 00:00:57 -0600

    added UI + logic

--- app/gameplay/page.jsx
+++ app/gameplay/page.jsx
@@ -8,21 +8,147 @@ export default function Counter() {
     setNumber(a => a + 1);
   }
 
+  function double() {
+    setNumber(a => a*2);
+  }
+
   return (
       <div className='flex flex-col items-center justify-center w-full font-semibold'>
         <h1>Number: {number}</h1>
 
-        <div className='flex-row py-2'>
+        <div id="row1-buttons" className='flex-row py-2'>
+
+
+
+<button className='px-4 py-2 mr-1 font-bold text-white bg-orange-400 rounded-full hover:bg-orange-700' onClick={() => {
+        increment();
+        increment();
+      }}>+2</button>
+
+            
       <button className='px-4 py-2 mr-1 font-bold text-white bg-orange-400 rounded-full hover:bg-orange-700' onClick={() => {
         increment();
         increment();
         increment();
       }}>+3</button>
-      <button className='px-4 py-2 font-bold text-white bg-orange-400 rounded-full hover:bg-orange-700' onClick={() => {
+
+<button className='px-4 py-2 mr-1 font-bold text-white bg-orange-400 rounded-full hover:bg-orange-700' onClick={() => {
         increment();
-      }}>+1</button>
+        increment();
+        increment();
+        increment();
+      }}>+4</button>
+
+<button className='px-4 py-2 mr-1 font-bold text-white bg-orange-400 rounded-full hover:bg-orange-700' onClick={() => {
+        increment();
+        increment();
+        increment();
+        increment();
+        increment();
+      }}>+5</button>
 
 </div>
+
+<div id="row2-buttons" className='flex-row py-2'>
+
+<button className='px-4 py-2 mr-1 font-bold text-white bg-orange-400 rounded-full hover:bg-orange-700' onClick={() => {
+        increment();
+        increment();
+        increment();
+        increment();
+        increment();
+        increment();
+      }}>+6</button>
+
+
+<button className='px-4 py-2 mr-1 font-bold text-white bg-orange-400 rounded-full hover:bg-orange-700' onClick={() => {
+        increment();
+        increment();
+        increment();
+        increment();
+        increment();
+        increment();
+        increment();
+      }}>+7</button>
+
+<button className='px-4 py-2 mr-1 font-bold text-white bg-orange-400 rounded-full hover:bg-orange-700' onClick={() => {
+        increment();
+        increment();
+        increment();
+        increment();
+        increment();
+        increment();
+        increment();
+        increment();
+      }}>+8</button>
+
+<button className='px-4 py-2 mr-1 font-bold text-white bg-orange-400 rounded-full hover:bg-orange-700' onClick={() => {
+        increment();
+        increment();
+        increment();
+        increment();
+        increment();
+        increment();
+        increment();
+        increment();
+        increment();
+      }}>+9</button>
+</div>
+
+<div id="row3-buttons" className='flex-row py-2'>
+
+<button className='px-4 py-2 mr-1 font-bold text-white bg-orange-400 rounded-full hover:bg-orange-700' onClick={() => {
+        increment();
+        increment();
+        increment();
+        increment();
+        increment();
+        increment();
+        increment();
+        increment();
+        increment();
+        increment();
+      }}>+10</button>
+
+<button className='px-4 py-2 mr-1 font-bold text-white bg-orange-400 rounded-full hover:bg-orange-700' onClick={() => {
+        increment();
+        increment();
+        increment();
+        increment();
+        increment();
+        increment();
+        increment();
+        increment();
+        increment();
+        increment();
+        increment();
+      }}>+11</button>
+
+<button className='px-4 py-2 mr-1 font-bold text-white bg-orange-400 rounded-full hover:bg-orange-700' onClick={() => {
+        increment();
+        increment();
+        increment();
+        increment();
+        increment();
+        increment();
+        increment();
+        increment();
+        increment();
+        increment();
+        increment();
+        increment();
+      }}>+12</button>
+
+<button className='px-4 py-2 mr-1 font-bold text-white bg-orange-400 rounded-full hover:bg-orange-700' onClick={() => {
+        double();
+      }}>x2</button>
+
+
+
+</div>
+
+
+
     </div>
   );
 }
