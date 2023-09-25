02edd777a68f990e7ae5ce4e2afa605776859720
JHoang <JHoang6900@gmail.com>
2023-09-25 00:57:38 -0600

    setup proper playerArray along with playerObjects

--- app/players/page.jsx
+++ app/players/page.jsx
@@ -2,21 +2,29 @@
 import { useState } from "react";
 
 export default function Counter() {
-  let [newPlayer, setNewPlayer] = useState("");
   const [playersArray, setPlayersArray] = useState([]);
+  const [newPlayerName, setNewPlayerName] = useState("");
+
+ 
 
   function addPlayer() {
     event.preventDefault();
 
+    if(newPlayerName.trim() !== "") {
+
+    const newPlayer = { name: newPlayerName, score: 0 };
+
     setPlayersArray([...playersArray, newPlayer]);
-    setNewPlayer("");
+    
 
-    playersArray.push(newPlayer);
-    console.log("player Added!", playersArray);
+    // playersArray.push(newPlayerName);
+    setNewPlayerName("");
+    console.log("player Added!");
   }
+}
 
-  function updatePlayerState(event) {
-    setNewPlayer(event.target.value);
+  function updatePlayerName(event) {
+    setNewPlayerName(event.target.value);
   }
 
   function removePlayer() {
@@ -30,31 +38,36 @@ export default function Counter() {
     }
   }
 
+  function debugPlayer() {
+    console.log('playersArray ~>',playersArray);
+  }
+
   return (
     <div className="flex flex-col items-center justify-center w-full p-10 mt-5 rounded-xl outline-double outline-zinc-300">
       <h1 className="font-bold">PLAYERS: </h1>
 
       <ul>
-        {playersArray.map((player, index) => (
-          <li key={index}>
-            {" "}
-            {player}
-            <button
-              className="px-1 py-0 ml-1 mr-1 font-bold text-white bg-red-700 rounded-xl hover:bg-red-900"
-              onClick={removePlayer}
-            >
-              -
-            </button>
-          </li>
-        ))}
+      {playersArray.map((player, index) => (
+        <li key={index} className="flex flex-row p-1.5 border rounded-lg border-zinc-400 m-1">
+          <p className="mr-2 text-white">{player.name}</p>
+          <p className="ml-2 mr-2 text-orange-200">{player.score}</p>
+
+          <button
+            className="px-1 py-0 ml-1 mr-1 font-bold text-white bg-red-700 rounded-xl hover:bg-red-900"
+            onClick={removePlayer}>-</button>
+            
+        </li>
+         ))}
       </ul>
+
+     
 
       <form>
         <input
           type="text"
           placeholder="Player Name"
-          value={newPlayer}
-          onChange={updatePlayerState}
+          value={newPlayerName}
+          onChange={updatePlayerName}
         ></input>
 
         <button
@@ -65,6 +78,24 @@ export default function Counter() {
           +
         </button>
       </form>
+
+
+      {/* <button
+        className="px-24 py-2 mr-1 font-bold text-white bg-green-800 rounded-full hover:bg-orange-700"
+        onClick={startGame}
+      >
+        Start!
+
+      startGame should be a function that hides the setup UI elements. 
+      </button> */}
+
+
+      <button
+        className="px-24 py-2 mr-1 font-bold text-white bg-green-800 rounded-full hover:bg-orange-700"
+        onClick={debugPlayer}
+      >
+        Debug!
+      </button>
     </div>
   );
 }
